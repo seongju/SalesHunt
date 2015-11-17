9cd26844ab82507cfecd105ba05aa120becb6224
SeongJu <seongju@umich.edu>
2015-11-16 21:32:45 -0500

    CORs works. Just forgot to push it before

--- amazonAPIHeroku/index.js
+++ amazonAPIHeroku/index.js
@@ -3,10 +3,10 @@ var express = require('express'),
  	bodyParser = require('body-parser'),
  	app = express();
 
-app.all('/', function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
-	next();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 app.use(bodyParser.json());
