794af643c7d963e6cccb73fb466b2811da4e3e62
SeongJu <seongju@umich.edu>
2015-12-01 18:54:20 -0500

    Got /addItem to work with Parse, I'm going to clean up code before I push to Heroku

--- amazonAPIHeroku/index.js
+++ amazonAPIHeroku/index.js
@@ -53,23 +53,31 @@ app.post('/removeItem', function(req, res){
 
 	var user_query = new Parse.Query(Parse.User);
 	
+	//Get the user pointer for this username
 	user_query.equalTo("username", username);
 	user_query.first({
 		success: function(user){
-			console.log(user);
+			
+			if(typeof user === "undefined"){
+				//There is no such user
+				res.status(400).send();
+			}
 
 			var userItem = Parse.Object.extend("User_Items");
 			var items_query = new Parse.Query(userItem);
 			
+			//Retrieve the unique item that has the ASIN and user pointer
 			items_query.equalTo("user", user);
 			items_query.equalTo("ASIN", ASIN);
 
 			items_query.first({
 				success: function(item){
 					if(typeof item === "undefined"){
+						//There is no such item 
 						res.status(400).send();
 					}
 					else{
+						//Remove item from database
 						item.destroy({
 							success: function(){
 								res.status(200).send();
@@ -92,5 +100,51 @@ app.post('/removeItem', function(req, res){
 });
 
 app.post('/addItem', function(req, res){
+	var username = req.body.username;
+	var item = req.body.item;
 
+	//Get the user pointer for this username
+	var user_query = new Parse.Query(Parse.User);
+
+	user_query.equalTo("username", username);
+	user_query.first({
+		success: function(user){
+			
+			if(typeof user === "undefined"){
+				//There is no such user
+				console.log("There is no such user");
+				res.status(400).send();
+			}
+
+			//Create new item and set the fields for it
+			var userItem = Parse.Object.extend("User_Items");
+			var newItem = new userItem();
+			
+			newItem.set("user", user);
+			newItem.set("ASIN", item['ASIN']);
+			newItem.set("brand", item['brand']);
+			newItem.set("lastPriceAmount", parseFloat(item['lastPriceAmount']));
+			newItem.set("lastPriceFormatted", item['lastPriceFormatted']);
+			newItem.set("pictureLink", item['pictureLink']);
+			newItem.set("setPriceAmount", parseFloat(item['setPriceAmount']));
+			newItem.set("setPriceFormatted", item['setPriceFormatted']);
+			newItem.set("title", item['title']);
+
+			console.log(newItem);
+			//Try to save the new item in Parse
+			newItem.save(null, {
+				success: function(){
+					res.status(200).send();
+				},
+				error: function(){
+					console.log("Trouble saving the item");
+					res.status(400).send();
+				}
+			});
+		},
+		error: function(){
+			console.log("Bad request for user");
+			res.status(400).send();
+		}
+	});
 });
