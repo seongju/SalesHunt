37212dc2720f38f513562abec1d829a24caf899a
Zijie Ku <kuzijie@umich.edu>
2015-12-05 15:20:06 -0500

    twill

--- saleshunt-ui/www/js/app.js
+++ saleshunt-ui/www/js/app.js
@@ -7,111 +7,111 @@
 // 'starter.controllers' is found in controllers.js
 // API_KEY: AIzaSyAi37LkflJ-QKBm0WW6UKLvZ_CA68e7yZk
 
-angular.module('starter', ['ionic','ionic.service.core', 'ngCordova', 'ionic.service.push', 'starter.controllers', 'starter.services', 'starter.keys'])
+angular.module('starter', ['ionic','starter.controllers', 'starter.services', 'starter.keys'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    /* external */
-    pushNotification = window.plugins.pushNotification;
+    // /* external */
+    // pushNotification = window.plugins.pushNotification;
     
-    window.onNotification = function(e){
+    // window.onNotification = function(e){
 
-          switch(e.event){
-            case 'registered':
-              if(e.regid.length > 0){
+    //       switch(e.event){
+    //         case 'registered':
+    //           if(e.regid.length > 0){
 
-                var device_token = e.regid;
-                RequestsService.register(device_token).then(function(response){
-                  alert('registered!');
-                });
-              }
-            break;
+    //             var device_token = e.regid;
+    //             RequestsService.register(device_token).then(function(response){
+    //               alert('registered!');
+    //             });
+    //           }
+    //         break;
 
-            case 'message':
-              alert('msg received: ' + e.message);
-              /*
-                {
-                    "message": "Hello this is a push notification",
-                    "payload": {
-                        "message": "Hello this is a push notification",
-                        "sound": "notification",
-                        "title": "New Message",
-                        "from": "813xxxxxxx",
-                        "collapse_key": "do_not_collapse",
-                        "foreground": true,
-                        "event": "message"
-                    }
-                }
-              */
-            break;
+    //         case 'message':
+    //           alert('msg received: ' + e.message);
+              
+    //             {
+    //                 "message": "Hello this is a push notification",
+    //                 "payload": {
+    //                     "message": "Hello this is a push notification",
+    //                     "sound": "notification",
+    //                     "title": "New Message",
+    //                     "from": "813xxxxxxx",
+    //                     "collapse_key": "do_not_collapse",
+    //                     "foreground": true,
+    //                     "event": "message"
+    //                 }
+    //             }
+              
+    //         break;
 
-            case 'error':
-              alert('error occured');
-            break;
+    //         case 'error':
+    //           alert('error occured');
+    //         break;
 
-          }
-    };
+    //       }
+    // };
 
-    window.errorHandler = function(error){
-      //alert('an error occured');
-    };
+    // window.errorHandler = function(error){
+    //   //alert('an error occured');
+    // };
 
-    pushNotification.register(
-      onNotification,
-      errorHandler,
-      {
-        'badge': 'true',
-        'sound': 'true',
-        'alert': 'true',
-        'ecb': 'onNotification',
-        'senderID': 'eecs441',
-        'device_token' : '1234'
-      }
-    );
+    // pushNotification.register(
+    //   onNotification,
+    //   errorHandler,
+    //   {
+    //     'badge': 'true',
+    //     'sound': 'true',
+    //     'alert': 'true',
+    //     'ecb': 'onNotification',
+    //     'senderID': 'eecs441',
+    //     'device_token' : '1234'
+    //   }
+    // );
 
+    // /* end of external */
     /* end of external */
-    /* end of external */
 
-    Ionic.io();
+    //Ionic.io();
 
-    var push = new Ionic.Push({
-      "debug": true,
-      "onNotification": function(notification) {
-        var payload = notification.payload;
-        // console.log(notification, payload);
-        alert( "Price drop on Macbook Air!");
-      },
-      "onRegister": function(data) {
-        // console.log(data.token);
+    // var push = new Ionic.Push({
+    //   "debug": true,
+    //   "onNotification": function(notification) {
+    //     var payload = notification.payload;
+    //     // console.log(notification, payload);
+    //     alert( "Price drop on Macbook Air!");
+    //   },
+    //   "onRegister": function(data) {
+    //     // console.log(data.token);
 
-      },
-      "pluginConfig": {
-        "ios": {
-          "badge": true,
-          "sound": true
-         },
-         "android": {
-           "iconColor": "#343434"
-         }
-      }
-    });
+    //   },
+    //   "pluginConfig": {
+    //     "ios": {
+    //       "badge": true,
+    //       "sound": true
+    //      },
+    //      "android": {
+    //        "iconColor": "#343434"
+    //      }
+    //   }
+    // });
     // this will give you a fresh user or the previously saved 'current user'
-    var user = Ionic.User.current();
-    user.id = Ionic.User.anonymousId();
-    // if the user doesn't have an id, you'll need to give it one.
-    if (!user.id) {
-      user.id = Ionic.User.anonymousId();
-      // user.id = 'your-custom-user-id';
-    }
+    // var user = Ionic.User.current();
+    // user.id = Ionic.User.anonymousId();
+    // // if the user doesn't have an id, you'll need to give it one.
+    // if (!user.id) {
+    //   user.id = Ionic.User.anonymousId();
+    //   // user.id = 'your-custom-user-id';
+    // }
 
-    var callback = function(pushToken) {
-      console.log('Registered token:', pushToken.token);
-      user.addPushToken(pushToken.token);
-      user.set('token', pushToken.token);
-      user.save(); // you NEED to call a save after you add the token
-    };
+    // var callback = function(pushToken) {
+    //   console.log('Registered token:', pushToken.token);
+    //   user.addPushToken(pushToken.token);
+    //   user.set('token', pushToken.token);
+    //   user.save(); // you NEED to call a save after you add the token
+    // };
 
-    push.register(callback);
+    // push.register(callback);
 
 
     
